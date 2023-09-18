2ad6bea7e88cb80f4b984c4106d9d6dea373d00d
Oleksandr <sanyagolikov97@gmail.com>
2023-09-18 23:26:24 +0300

    Login method

--- UserAuthWebAPI/UserAuthJWT/Controllers/AuthController.cs
+++ UserAuthWebAPI/UserAuthJWT/Controllers/AuthController.cs
@@ -10,13 +10,29 @@ public class AuthController : ControllerBase
 {
     public static User user = new User();
 
-    [HttpPost("Register")]
+    [HttpPost("register")]
     public ActionResult<User> Register(UserDto request)
     {
         string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
 
         user.Username = request.Username;
         user.PasswordHash = passwordHash;
+
+        return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public ActionResult<User> Login(UserDto request)
+    {
+        if (user.Username != request.Username)
+        {
+            return BadRequest("User not found");
+        }
+
+        if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+        {
+            return BadRequest("Wrong password");
+        }
 
         return Ok(user);
     }
